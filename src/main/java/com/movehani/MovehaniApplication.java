4ec464a2b6f34610289909cc168c82fb579c551c
mirasoy <nkskjjbb@gmail.com>
2022-06-23 23:52:36 +0900

    first commit

---
+++ src/main/java/com/movehani/MovehaniApplication.java
@@ -0,0 +1,13 @@
+package com.movehani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovehaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovehaniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movehani/MovehaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movehani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovehaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
